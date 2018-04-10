3061280c4dbc4a7dbd6ae476abec3e47b611baf4
ricoarisandyw <arisandyrico@gmail.com>
2018-04-10 10:09:40 +0700

    First Commit

---
+++ ricolib/src/main/java/com/javacreativeapps/ricolib/Personal.java
@@ -0,0 +1,53 @@
+package com.javacreativeapps.ricolib;
+
+/**
+ * Created by wijaya on 4/10/2018.
+ */
+
+public class Personal {
+    String name;
+    String role;
+    String skill;
+    int hp;
+    boolean magic;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isMagic() {
+        return magic;
+    }
+
+    public void setMagic(boolean magic) {
+        this.magic = magic;
+    }
+}
